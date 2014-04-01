0db0554854beab9b6192b14c0818b6c1a48bbdc3
Daniel Bastos <danielfloresbastos@gmail.com>
2014-04-01 13:14:04 -0300

    Exercício da Tombola, faltou apenas finalizar a implementação do objeto iteravel

--- exercicios/tombola/tombola.py
+++ exercicios/tombola/tombola.py
@@ -6,11 +6,15 @@ class Tombola(object):
     def carregar(self, seq):
         self.itens = list(seq)
 
-    def misturar(self):
-        shuffle(self.itens)
+    def misturar(self, pairswap=None):
+        pairswap(self.itens) if pairswap else shuffle(self.itens)
 
     def sortear(self):
         return self.itens.pop()
 
     def carregada(self):
         return bool(self.itens)
+
+    def __iter__(self):
+        for i in self.itens:
+            yield i
